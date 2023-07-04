8deb1c9f3b4d171133afc1d8a01faa16afb5f037
wildan <muhammad.wildan@meteor.id>
2023-07-04 10:50:10 +0700

    add crousel base on ssearch

--- src/components/PageGroups/Home.tsx
+++ src/components/PageGroups/Home.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 import BannerContent from '../PageSections/Hero';
 import Category from '../PageSections/category/Category';
 import ProductList from '../PageSections/products';
+import BaseOnSearch from '../PageSections/baseOnSearch';
 
 const Home = () => {
   return (
     <div className="mx-auto max-w-7xl xl:px-10 px-3 py-5">
       <BannerContent />
       <Category />
+      <BaseOnSearch />
       <ProductList />
     </div>
   );
