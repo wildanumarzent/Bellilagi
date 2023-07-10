458e4a224f49b94c6ff03351d48eb1f04063c32a
wildan <muhammad.wildan@meteor.id>
2023-07-10 19:46:19 +0700

    deploy json.placholder

--- src/components/PageSections/baseOnDiscount/index.tsx
+++ src/components/PageSections/baseOnDiscount/index.tsx
@@ -1,5 +1,6 @@
 import CardDiscount from '../products/component/CardDiscount';
 import useProducts from '@/store/products';
+import clsx from 'clsx';
 
 const BaseOnDiscount = () => {
   const { products, getProducts } = useProducts();
@@ -24,7 +25,7 @@ const BaseOnDiscount = () => {
           <img
             src="https://images.tokopedia.net/img/cache/240/zssuBf/2023/6/22/fcc2526b-1ee8-4cd8-bf8e-61d1dfbe1660.png.webp?ect=4g"
             alt="image"
-            className="w-[280px]"
+            className={clsx(products.length > 1 ? 'w-[280px] h-[320px]' : 'w-[130px] h-[320px]')}
           />
         </div>
         <div className="carousel rounded-box gap-3 mt-5 -ml-32">
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,2 +1,3 @@
-export const API_BASE_URL = 'http://localhost:3004';
+export const API_BASE_URL =
+  'https://my-json-server.typicode.com/wildanumarzent/dumy-data-ecommerce';
 export const API_REQERES_URL = 'https://reqres.in';
